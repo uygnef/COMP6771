9c29d9e5f13d7bd95ca55339aac2cf87f485a71d
Yu Feng <97508691@qq.com>
2017-07-30 23:29:21 +1000

    add sub div multi completed.

---
+++ a1/main.h
@@ -0,0 +1,12 @@
+//
+// Created by yu on 30/07/17.
+//
+void parse_token (std::string line);
+int handle_number(std::string input);
+int arithmetic(std::string input);
+
+
+struct number{
+    double value;
+    bool is_int;
+};
