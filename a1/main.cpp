2be76c28eecd83b5478672dc0c1dc0bf8718bdfa
Yu Feng <97508691@qq.com>
2017-08-01 11:22:11 +1000

    reverse

--- a1/main.cpp
+++ a1/main.cpp
@@ -25,26 +25,39 @@ int main(int argc, char* argv[]) {
 }
 
 
-
 void parse_token(std::string input){
 
     //handle float
 
-    if(!handle_number(input)){
-        if((input.compare("add") == 0 || input.compare("sub") == 0
-             || input.compare("mult") == 0|| input.compare("div") == 0)){
-            arithmetic(input);
-        }
+    if(!handle_number(input)) {
+        return;
     }
 
+    if(input == "add" || input == "sub" || input == "mult" || input == "div"){
+        arithmetic(input);
+        return;
+    }
 
+    if(input == "reverse"){
+        auto depth = int(stack.top().value);
+        stack.pop();
+        struct number queue[depth];
+        int i;
+        for(i = 0; i < depth; i++){
+            queue[i] = stack.top();
+            stack.pop();
+        }
 
+        for(auto j: queue){
+            stack.push(j);
+        }
+    }
 }
 
-int handle_number(std::string input){
+bool handle_number(std::string input){
 
-    if (isdigit(input[0])){
-        struct number new_number;
+    if (isdigit(input[0]) == 0){
+        struct number new_number{};
         //handle double
         if (input.find('.') != std::string::npos){
             new_number.is_int = false;
@@ -55,36 +68,36 @@ int handle_number(std::string input){
             new_number.is_int = true;
         }
         stack.push(new_number);
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
 
 int arithmetic(std::string input) {
 
-    struct number x,y,result;
-    x = stack.top();
+    struct number x = stack.top();
     stack.pop();
-    y = stack.top();
+    struct number y = stack.top();
     stack.pop();
+    struct number result{};
     result.is_int = x.is_int && y.is_int;
 
 
     std::cout << (x.is_int ? std::to_string(int(x.value)) : std::to_string(x.value)) ;
 
-    if(input.compare("add") == 0 ){
+    if(input == "add"){
         std::cout << " + ";
         result.value = x.value + y.value;
     }
-    if(input.compare("sub") == 0){
+    if(input == "sub"){
         std::cout << " - ";
         result.value = x.value - y.value;
     }
-    if(input.compare("mult") == 0){
+    if(input == "mult"){
         std::cout << " * ";
         result.value = x.value * y.value;
     }
-    if(input.compare("div") == 0){
+    if(input == "div"){
         std::cout << " / ";
         result.value = x.value / y.value;
     }
--- a1/main.h
+++ a1/main.h
@@ -2,7 +2,7 @@
 // Created by yu on 30/07/17.
 //
 void parse_token (std::string line);
-int handle_number(std::string input);
+bool handle_number(std::string input);
 int arithmetic(std::string input);
 
 
